75f22feeab00e4c8659a4234a53cb285890b7690
Smit Valture <smitvalture@gmail.com>
2023-03-11 23:17:04 +0530

    popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from "react";
 import Login from "./pages/Login";
 import Landing from "./pages/Landing";
 import Signup from "./pages/Signup";
 import { Routes, Route } from "react-router-dom";
-import Dash_sidebar from "./components/Dash_sidebar";
 import Dashboard from "./components/Dashboard";
 import Biz_dash from "./components/Biz_dash";
 import Bill_dash from "./components/Bill_dash";
 import Contact_dash from "./components/Contact_dash";
 import Refer_dash from "./components/Refer_dash";
-import Emp_list from "./components/Emp_list";
-import AddEmpPop from "./components/AddEmpPop";
+
 
 function App() {
   return (
@@ -26,6 +23,7 @@ function App() {
         <Route path='/billing' element={<Bill_dash />} />
         <Route path='/contact-us' element={<Contact_dash />} />
         <Route path='/refer' element={<Refer_dash />} />
+        
       </Routes>
     </div>
   );
